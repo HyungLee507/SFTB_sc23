265675c46bf01dc4123e2966357018c5a85d0457
leedonghyun <skydh507@ajou.ac.kr>
2023-11-03 04:14:13 +0900

    feat : 소셜 로그인 연동

--- vue-cli/src/components/LoginPage.vue
+++ vue-cli/src/components/LoginPage.vue
@@ -2,34 +2,36 @@
   <div class="container d-flex justify-content-center">
     <b-form @submit="submitForm" v-if="show">
       <b-form-group
-        id="input-group-1"
-        label="이메일"
-        label-for="input-1"
+          id="input-group-1"
+          label="이메일"
+          label-for="input-1"
       >
         <b-form-input
-          type="email"
-          v-model="form.email"
-          id="input-1"
-          aria-describedby="email-help-block"
-          required
+            type="email"
+            v-model="form.email"
+            id="input-1"
+            aria-describedby="email-help-block"
+            required
         ></b-form-input>
       </b-form-group>
 
       <b-form-group id="input-group-2" label="비밀번호" label-for="input-2">
-        <b-form-input type="password" v-model="form.password" id="input-2" aria-describedby="password-help-block" required></b-form-input>
+        <b-form-input type="password" v-model="form.password" id="input-2" aria-describedby="password-help-block"
+                      required></b-form-input>
       </b-form-group>
 
-      <b-button type="submit" variant="primary">로그인</b-button> 
+      <b-button type="submit" variant="primary">로그인</b-button>
       <b-button to="/account/join" variant="primary">회원가입</b-button>
       <b-button to="/account/findpassword" variant="primary">비밀번호 찾기</b-button>
-      
+      <b-button @click="loginWithNaver" variant="primary">네이버 회원가입</b-button>
+
 
       <!-- Error message display -->
       <div v-if="error" class="alert alert-danger">
         {{ error }}
       </div>
     </b-form>
-    <button href="">img(src="../assets/img/naverlogo.png")</button>
+    <button href="/oauth2/authorization/naver"><img src="vue-cli/src/assets/naverlogo.png"></button>
   </div>
 </template>
 
@@ -52,23 +54,38 @@ export default {
   },
   methods: {
     async submitForm() {
-      
-        await axios.post('/account/login', this.form)
-        .then((res) => {
-          if(res.status===200){
-            console.log(res.data);
-            this.token = this.res.data.data.token;
-            console.log("토큰:"+this.token);
-            this.$cookie.set("accesstoken",res.data.data.token,1);
-            axios.defaults.headers.common["x-access-token"] = res.data.data.token;
-            alert("로그인 성공");
-            router.push("/");
-          }
-        }).catch((err)=>{
-          alert("로그인 실패" + err)
-        });
+
+      await axios.post('/account/login', this.form)
+          .then((res) => {
+            if (res.status === 200) {
+              console.log(res.data);
+              this.token = this.res.data.data.token;
+              console.log("토큰:" + this.token);
+              this.$cookie.set("accesstoken", res.data.data.token, 1);
+              axios.defaults.headers.common["x-access-token"] = res.data.data.token;
+              alert("로그인 성공");
+              router.push("/");
+            }
+          }).catch((err) => {
+            alert("로그인 실패" + err)
+          });
     },
-  },
-  
+    //   sendGetRequest() {
+    //     axios.get('http://localhost:8080/oauth2/authorization/naver')
+    //         .then(response => {
+    //           // GET 요청에 대한 응답을 처리하세요
+    //           console.log(response);
+    //         })
+    //         .catch(error => {
+    //           // 오류 처리
+    //           console.error(error);
+    //         });
+    //   },
+    //
+    // }
+    loginWithNaver() {
+      window.location.href = '/oauth2/authorization/naver';
+    }
+  }
 };
 </script>
