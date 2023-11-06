c4386395d13e5882628560503024b9fda6106a7e
leedonghyun <skydh507@ajou.ac.kr>
2023-11-06 21:20:48 +0900

    feat : cors 리소스 접근

--- src/main/java/CDProject/vfmarket/global/config/WebConfig.java
+++ src/main/java/CDProject/vfmarket/global/config/WebConfig.java
@@ -1,8 +1,13 @@
 package CDProject.vfmarket.global.config;
 
+import java.io.IOException;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -11,5 +16,21 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")  // Vue.js 애플리케이션의 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**/*")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath,
+                                                   Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                : new ClassPathResource("/static/index.html");
+                    }
+                });
     }
 }
