486c070f94e2679a1fddb5f0ece25c4848e35600
leedonghyun <skydh507@ajou.ac.kr>
2023-11-15 19:58:10 +0900

    feat : jwt claims 가져오는 기능 구현

---
+++ src/main/java/CDProject/vfmarket/global/jwt/TokenValueProvider.java
@@ -0,0 +1,20 @@
+package CDProject.vfmarket.global.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TokenValueProvider {
+    @Value("${jwt.secretKey}")
+    private String secretKey;
+
+    public Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
