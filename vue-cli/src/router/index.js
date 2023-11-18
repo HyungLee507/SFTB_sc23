8c6366ce1fbd8bd03f693ef0abd1689460261e7d
leedonghyun <skydh507@ajou.ac.kr>
2023-11-18 22:05:48 +0900

    vue 라우터 구성

--- vue-cli/src/router/index.js
+++ vue-cli/src/router/index.js
@@ -7,6 +7,13 @@ import ProductregistrationPage from '@/components/ProductregistrationPage'
 import ProductinformationPage from '@/components/ProductinformationPage'
 import ProductlistPage from '@/components/ProductlistPage'
 import UserInputPage from "@/components/UserInputPage.vue";
+import CartPage from "@/components/CartPage.vue";
+import MyPage from "@/components/MyPage.vue";
+import ProfilePage from "@/components/ProfilePage.vue";
+import VirtualFittingPage from "@/components/VirtualFittingPage.vue";
+import OrdersPage from "@/components/OrdersPage.vue";
+import SalesHistoryPage from "@/components/SalesHistoryPage.vue";
+import SellingPage from "@/components/SellingPage.vue";
 
 
 Vue.use(Router); // VueRouter를 사용하겠다.
@@ -31,7 +38,7 @@ export default new Router({
             component: ProductregistrationPage,
         },
         {
-            path: '/product/list/:id',
+            path: '/product-list/:id',
             component: ProductinformationPage
         },
         {
@@ -41,6 +48,34 @@ export default new Router({
         {
             path: '/user/input',
             component: UserInputPage,
+        },
+        {
+            path: '/user/cart',
+            component: CartPage,
+        },
+        {
+            path: '/user/mypage',
+            component: MyPage,
+        },
+        {
+            path: '/user/mypage/profile',
+            component: ProfilePage,
+        },
+        {
+            path: '/user/mypage/orders',   //없앨지말지
+            component: OrdersPage,
+        },
+        {
+            path: '/user/mypage/virtualfitting',
+            component: VirtualFittingPage,
+        },
+        {
+            path: '/user/mypage/saleshistory',
+            component: SalesHistoryPage,
+        },
+        {
+            path: '/user/mypage/saleshistory/selling',
+            component: SellingPage,
         },
 
 
