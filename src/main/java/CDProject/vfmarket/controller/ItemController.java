83cf86339c21122a06b62eeb0ae8d5965e16a30a
leedonghyun <skydh507@ajou.ac.kr>
2023-11-24 01:59:34 +0900

    feat : 판매중인 상품 조회 기능

--- src/main/java/CDProject/vfmarket/controller/ItemController.java
+++ src/main/java/CDProject/vfmarket/controller/ItemController.java
@@ -2,7 +2,10 @@ package CDProject.vfmarket.controller;
 
 import CDProject.vfmarket.domain.dto.itemDTO.ItemDetailDto;
 import CDProject.vfmarket.domain.dto.itemDTO.ItemViewDto;
+import CDProject.vfmarket.domain.dto.itemDTO.SalesItemDto;
+import CDProject.vfmarket.global.jwt.TokenValueProvider;
 import CDProject.vfmarket.service.ItemService;
+import io.jsonwebtoken.Claims;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,16 +15,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-//@EnableWebSecurity
+//@EnableMethodSecurity
 public class ItemController {
 
     private final ItemService itemService;
+    private final TokenValueProvider tokenValueProvider;
 
     @GetMapping("/product-list")
 //    @PreAuthorize("isAuthenticated()")
@@ -34,6 +39,7 @@ public class ItemController {
                 uniqueItemsMap.put(dto.getId(), dto);
             }
         }
+        log.info("value is = {}", uniqueItemsMap.values());
         return new ArrayList<>(uniqueItemsMap.values());
     }
 
@@ -44,6 +50,16 @@ public class ItemController {
     @GetMapping("/product-list/{itemId}")
     public ItemDetailDto itemDetail(@PathVariable Long itemId) {
         return itemService.itemDetailInfo(itemId);
+    }
+
+    @GetMapping("/salesItems")
+    public List<SalesItemDto> salesItems(@RequestHeader("Authorization") String token) {
+        String trim = token.replace("Bearer ", "");
+        Claims claims = tokenValueProvider.extractClaims(trim);
+        long sellerId = Long.parseLong(claims.get("userId").toString());
+        List<SalesItemDto> salesItems = itemService.getSalesItems(sellerId);
+        log.info("sales Items ={}", salesItems);
+        return salesItems;
     }
 
 
