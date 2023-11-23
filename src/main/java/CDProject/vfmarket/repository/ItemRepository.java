395b0b142bcc0b4b38d431f24b8388a52ae12dd6
leedonghyun <skydh507@ajou.ac.kr>
2023-11-24 01:59:26 +0900

    feat : 판매중인 상품 조회 기능

--- src/main/java/CDProject/vfmarket/repository/ItemRepository.java
+++ src/main/java/CDProject/vfmarket/repository/ItemRepository.java
@@ -9,6 +9,15 @@ import org.springframework.data.jpa.repository.Query;
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
 
+    List<Item> findAllBySellerId(Long sellerId);
+
+//    @Query("select new CDProject.vfmarket.domain.dto.itemDTO.SalesItemDto(i.id,i.price,i.itemName,(SELECT image.fileName FROM i.images image WHERE image.item = i ORDER BY image.id ASC))"
+//            + " from Item i"
+//            + " left join i.images img"
+//            + " where i.status = CDProject.vfmarket.domain.entity.ItemStatus.FOR_SALE"
+//            + " AND i.sellerId = :sellerId")
+//    List<SalesItemDto> findAllSalesItems(@Param("sellerId") Long sellerId);
+
     @Query("select new CDProject.vfmarket.domain.dto.itemDTO.ItemViewDto(i.id,i.itemName,i.price,i.createdDate,img.fileName)"
             + " from Item i "
             + " left join i.images img"
