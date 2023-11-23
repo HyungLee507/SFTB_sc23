921681e963b506f0e9a8fe00d01b8a6ae728f00a
leedonghyun <skydh507@ajou.ac.kr>
2023-11-24 01:58:48 +0900

    feat : 결제 정보 저장 및 내역 조회 기능

--- src/main/java/CDProject/vfmarket/payment/PaymentController.java
+++ src/main/java/CDProject/vfmarket/payment/PaymentController.java
@@ -1,14 +1,22 @@
 package CDProject.vfmarket.payment;
 
+import CDProject.vfmarket.domain.dto.OrderDTO.OrderSaveDto;
+import CDProject.vfmarket.global.jwt.TokenValueProvider;
 import com.siot.IamportRestClient.IamportClient;
 import com.siot.IamportRestClient.response.IamportResponse;
 import com.siot.IamportRestClient.response.Payment;
+import io.jsonwebtoken.Claims;
 import jakarta.annotation.PostConstruct;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -20,9 +28,11 @@ public class PaymentController {
 
     private final PaymentService paymentService;
 
+    private final TokenValueProvider tokenValueProvider;
+
     private IamportClient iamportClient;
 
-    @Value("${portone.code}")
+    @Value("${portone.api.key}")
     private String apiKey;
 
     @Value("${portone.api.secretKey}")
@@ -33,29 +43,36 @@ public class PaymentController {
         this.iamportClient = new IamportClient(apiKey, secretKey);
     }
 
-    //해당 경로로 요청을 받으면 요청으로 받은 주문 상품들을 저장한다.
-//    @PostMapping("/payment")
-//    public ResponseEntity<String> paymentComplete(@RequestHeader("Authorization") String token,
-//                                                  @RequestBody List<OrderSaveDto> orderSaveDtos) throws IOException {
+    //    해당 경로로 요청을 받으면 요청으로 받은 주문 상품들을 저장한다.
+    @PostMapping("/payment")
+    public ResponseEntity<String> paymentComplete(@RequestHeader("Authorization") String token,
+                                                  @RequestBody OrderSaveDto orderSaveDto) throws IOException {
+        String trim = token.replace("Bearer ", "");
+        Claims claims = tokenValueProvider.extractClaims(trim);
+        long userId = Long.parseLong(claims.get("userId").toString());
+        String orderNumber = orderSaveDto.getMerchant_uid();
+        log.info("여기까진 오케이");
 //        String orderNumber = orderSaveDtos.get(0).getOrderNumber();
-//        try {
+        try {
 //            Long userId = sessionUser.getUserIdNo();
-//            paymentService.saveOrder(userId, orderSaveDtos);
+            paymentService.saveOrder(userId, orderSaveDto);
 //            log.info("결제 성공 : 주문 번호 {}", orderNumber);
-//            return ResponseEntity.ok().build();
-//        } catch (RuntimeException e) {
-//            log.info("주문 상품 환불 진행 : 주문 번호 {}", orderNumber);
-//            String impToken = refundService.getToken(apiKey, secretKey);
-//            refundService.refundRequest(impToken, orderNumber, e.getMessage());
-//            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-//        }
-//    }
+            return ResponseEntity.ok().build();
+        } catch (RuntimeException e) {
+            log.info("주문 상품 환불 진행 : 주문 번호 {}", orderNumber);
+            String impToken = refundService.getToken(apiKey, secretKey);
+            refundService.refundRequest(impToken, orderNumber, e.getMessage());
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 
-    //이게 결제 내역 조회인거네
+    //portOne 결제 내역 조회
     @PostMapping("/payment/validation/{imp_uid}")
     public IamportResponse<Payment> validateIamport(@PathVariable String imp_uid) {
         IamportResponse<Payment> payment = iamportClient.paymentByImpUid(imp_uid);
         log.info("결제 요청 응답. 결제 내역 - 주문 번호: {}", payment.getResponse().getMerchantUid());
+        log.info("결제 요청 응답. 결제 내역 - 주문 금액: {}", payment.getResponse().getAmount());
+        log.info("결제 요청 응답. 결제 내역 - 주문 금액: {}", payment.getResponse().getBuyerTel());
         return payment;
     }
 }
--- src/main/java/CDProject/vfmarket/payment/PaymentService.java
+++ src/main/java/CDProject/vfmarket/payment/PaymentService.java
@@ -1,6 +1,20 @@
 package CDProject.vfmarket.payment;
 
+import CDProject.vfmarket.domain.dto.OrderDTO.OrderSaveDto;
+import CDProject.vfmarket.domain.entity.Image;
+import CDProject.vfmarket.domain.entity.Item;
+import CDProject.vfmarket.domain.entity.ItemStatus;
+import CDProject.vfmarket.domain.entity.Order;
+import CDProject.vfmarket.domain.entity.OrderDetail;
+import CDProject.vfmarket.domain.entity.OrderStatus;
+import CDProject.vfmarket.domain.entity.User;
+import CDProject.vfmarket.repository.ItemRepository;
+import CDProject.vfmarket.repository.OrderDetailRepository;
+import CDProject.vfmarket.repository.OrderRepository;
+import CDProject.vfmarket.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,26 +24,65 @@ import org.springframework.stereotype.Service;
 @Service
 @Transactional
 public class PaymentService {
+    private final OrderDetailRepository orderDetailRepository;
 
-//    private final OrderRepository orderRepository;
-//    private final OrderProductRepository orderProductRepository;
-//    private final ProductRepository productRepository;
+    //ToDo : saveOrder-> 결제 성공시 결제 내역 생성
 
-//    public void saveOrder(Long userId, List<OrderSaveDto> orderSaveDtos) {
-//        OrderSaveDto saveDto = orderSaveDtos.get(0);
-//        Orders orders = Orders.builder()
-//                .userIdNo(userId)
-//                .orderNumber(saveDto.getOrderNumber())
-//                .receiverName(saveDto.getReceiverName())
-//                .phoneNumber(saveDto.getPhoneNumber())
-//                .zipcode(saveDto.getZipcode())
-//                .address(saveDto.getAddress())
-//                .orderRequired(saveDto.getOrderRequired())
-//                .orderStatus(PayStatus.SUCCESS.getStatus())
-//                .paymentMethod(saveDto.getPaymentMethod())
-//                .build();
+    private final OrderRepository orderRepository;
+
+    private final ItemRepository itemRepository;
+
+    private final UserRepository userRepository;
+
+    public void saveOrder(Long userId, OrderSaveDto orderSaveDto) {
+
+        Optional<Item> items = itemRepository.findById(orderSaveDto.getId());
+//        if(items.isPresent()){
+//            Item item = items.get();
+//        }
+        log.info("item sellerName is {}", items.get().getSellerName());
+        items.get().setStatus(ItemStatus.STOP_SELLING);
+        Optional<User> sellerUser = userRepository.findById(userId);
+        log.info("sellerUser name is {}", sellerUser.get().getName());
+        Image firstImage = getFirstImage(items.get().getId());
+
+        String fileName = firstImage.getFileName();
+        log.info("findImage name is {}", fileName);
+        try {
+            Order order = Order.builder()
+                    .paymentPrice(orderSaveDto.getPaid_amount())
+                    .sellerName(orderSaveDto.getSeller_name())
+                    .sellerId(items.get().getSellerId())
+                    .merchant_uid(orderSaveDto.getMerchant_uid())
+                    .status(OrderStatus.COMPLETE_PAYMENT)
+                    .representativeImage(fileName)
+                    .build();
+            if (userRepository.findById(userId).isPresent()) {
+                order.setBuyer(userRepository.findById(userId).get());
+            }
+            order.setItem(items.get());
+            orderRepository.save(order);
+            itemRepository.save(items.get());
+        } catch (Exception e) {
+            log.info("order error!!", e);
+        }
+        Order savedOrder = orderRepository.findByItem_Id(orderSaveDto.getId());
+        OrderDetail orderDetail = OrderDetail.builder()
+                .apply_num(orderSaveDto.getApply_num())
+                .buyer_addr(orderSaveDto.getBuyer_addr())
+                .buyer_email(orderSaveDto.getBuyer_email())
+                .buyer_name(orderSaveDto.getBuyer_name())
+                .buyer_postcode(orderSaveDto.getBuyer_postcode())
+                .card_name(orderSaveDto.getCard_name())
+                .card_number(orderSaveDto.getCard_number())
+                .pg_provider(orderSaveDto.getPg_provider())
+                .imp_uid(orderSaveDto.getImp_uid())
+                .build();
+        orderDetail.setOrder(savedOrder);
+        orderDetailRepository.save(orderDetail);
+
 //        orderRepository.insertOrder(orders);
-//
+
 //        for (OrderSaveDto dto : orderSaveDtos) {
 //            int curStock = productRepository.selectProductStock(dto.getProductId());
 //            int orderStock = dto.getOrderCount();
@@ -48,8 +101,11 @@ public class PaymentService {
 //                    .build();
 //            orderProductRepository.insertOrderProduct(orderProduct);
 //            productRepository.updateProductStock(dto.getProductId(), dto.getOrderCount());
-//        }
-//    }
+    }
 
+    public Image getFirstImage(Long itemId) {
+        Item item = itemRepository.findById(itemId).orElseThrow(() -> new EntityNotFoundException("Item not found"));
+        return item.getImages().isEmpty() ? null : item.getImages().get(0);
+    }
 
 }
