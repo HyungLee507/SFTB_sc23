11e5bd7e9d3b14f1f40cc97c47b43cbc7c07049a
leedonghyun <skydh507@ajou.ac.kr>
2023-11-27 05:15:09 +0900

    feat : security 인증 반영

---
+++ src/main/java/CDProject/vfmarket/global/filter/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,16 @@
+package CDProject.vfmarket.global.filter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "인증이 필요한 서비스입니다.");
+    }
+}
