69f1ed3ce6b8c53d39c5c2f1b361cad262a0062d
leedonghyun <skydh507@ajou.ac.kr>
2023-11-27 20:57:13 +0900

    feat : 댓글 조회

--- src/main/java/CDProject/vfmarket/repository/CommentRepository.java
+++ src/main/java/CDProject/vfmarket/repository/CommentRepository.java
@@ -1,9 +1,17 @@
 package CDProject.vfmarket.repository;
 
+import CDProject.vfmarket.domain.dto.commentDTO.CommentDto;
 import CDProject.vfmarket.domain.entity.Comment;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-
+    @Query("select new CDProject.vfmarket.domain.dto.commentDTO.CommentDto(c.id,u.id,u.name,c.description,c.writeStatus,c.lastModifiedDate)"
+            + " from Comment c"
+            + " left join c.user u"
+            + " left join c.review r"
+            + " where r.id = :reviewId")
+    List<CommentDto> findAllCommentsInReview(Long reviewId);
 }
