34ea333f1c503af48da8ca5607aea92ee7fc7d76
leedonghyun <skydh507@ajou.ac.kr>
2023-11-28 03:13:51 +0900

    알림 기능 사용 dto

---
+++ src/main/java/CDProject/vfmarket/domain/dto/NotificationDTO/NotificationViewDto.java
@@ -0,0 +1,16 @@
+package CDProject.vfmarket.domain.dto.NotificationDTO;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class NotificationViewDto {
+
+    private String message;
+
+    private LocalDateTime createdTime;
+
+    private boolean isRead;
+}
