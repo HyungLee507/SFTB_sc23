6f35fa339d16fcaf20bf1817f5ca3e5c8ca83a0c
leedonghyun <skydh507@ajou.ac.kr>
2023-11-29 02:48:17 +0900

    feat : 판매자 리뷰 목록 조회 기능

--- src/main/java/CDProject/vfmarket/service/ReviewService.java
+++ src/main/java/CDProject/vfmarket/service/ReviewService.java
@@ -1,16 +1,20 @@
 package CDProject.vfmarket.service;
 
+import CDProject.vfmarket.domain.dto.ReviewDTO.ReviewDto;
 import CDProject.vfmarket.domain.dto.ReviewDTO.ReviewFormDto;
 import CDProject.vfmarket.domain.dto.ReviewDTO.ReviewUpdateFormDto;
+import CDProject.vfmarket.domain.dto.commentDTO.CommentDto;
 import CDProject.vfmarket.domain.entity.Item;
 import CDProject.vfmarket.domain.entity.Review;
 import CDProject.vfmarket.domain.entity.User;
 import CDProject.vfmarket.domain.entity.WriteStatus;
+import CDProject.vfmarket.repository.CommentRepository;
 import CDProject.vfmarket.repository.ItemRepository;
 import CDProject.vfmarket.repository.ReviewRepository;
 import CDProject.vfmarket.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -20,22 +24,41 @@ import org.springframework.stereotype.Service;
 @Transactional
 @RequiredArgsConstructor
 public class ReviewService {
+
     private final ReviewRepository reviewRepository;
+
     private final UserRepository userRepository;
+
+    private final CommentRepository commentRepository;
 
     private final ItemRepository itemRepository;
 
-    public void saveReview(Long userId, ReviewFormDto reviewFormDto) {
-        User seller = userRepository.findById(userId)
-                .orElseThrow(() -> new EntityNotFoundException("User not found with id: " + userId));
+    public List<ReviewDto> getReviews(Long itemId) {
+        Long sellerId = itemRepository.findSellerIdByItemId(itemId);
+//        User seller = userRepository.findById(sellerId)
+//                .orElseThrow(() -> new EntityNotFoundException("User not found with id: " + sellerId));
+        List<ReviewDto> reviews = reviewRepository.findALlReviewBySellerId(sellerId);
+        for (ReviewDto reviewDto : reviews) {
+            List<CommentDto> comments = commentRepository.findAllCommentsInReview(reviewDto.getReviewId());
+            reviewDto.setComments(comments);
+        }
+        return reviews;
+    }
+
+    public void saveReview(Long reviewerId, ReviewFormDto reviewFormDto) {
         Item findItem = itemRepository.findById(reviewFormDto.getItemId())
                 .orElseThrow(() -> new EntityNotFoundException("Item not found with id: " + reviewFormDto.getItemId()));
+        User seller = userRepository.findById(findItem.getSellerId())
+                .orElseThrow(() -> new EntityNotFoundException("seller not found with id: " + findItem.getSellerId()));
+        User buyer = userRepository.findById(reviewerId)
+                .orElseThrow(() -> new EntityNotFoundException("buyer not found with id: " + reviewerId));
         Review uploadReview = Review.builder()
-                .user(seller)
+                .seller(seller)
+                .buyer(buyer)
                 .item(findItem)
                 .starRate(reviewFormDto.getStarRate())
                 .writeStatus(WriteStatus.REVIEW_AVAILABLE)
-                .reviewName(reviewFormDto.getName())
+                .title(reviewFormDto.getTitle())
                 .content(reviewFormDto.getContent())
                 .build();
         reviewRepository.save(uploadReview);
@@ -91,7 +114,7 @@ public class ReviewService {
     public void updateReview(Long reviewId, ReviewUpdateFormDto reviewUpdateFormDto) {
         Review review = reviewRepository.findById(reviewId)
                 .orElseThrow(() -> new EntityNotFoundException("Review not found with id: " + reviewId));
-        review.setReviewName(reviewUpdateFormDto.getReviewName());
+        review.setTitle(reviewUpdateFormDto.getReviewName());
         review.setStarRate(reviewUpdateFormDto.getStarRate());
         review.setContent(reviewUpdateFormDto.getContent());
         reviewRepository.save(review);
