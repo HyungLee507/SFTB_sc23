20b105210bd320869b04472fc590b9cdd6b3c82d
leedonghyun <skydh507@ajou.ac.kr>
2023-11-28 15:18:24 +0900

    fix : 알림 기능에 대한 연관관계 수정

--- src/main/java/CDProject/vfmarket/service/NotificationService.java
+++ src/main/java/CDProject/vfmarket/service/NotificationService.java
@@ -1,8 +1,11 @@
 package CDProject.vfmarket.service;
 
 import CDProject.vfmarket.domain.dto.NotificationDTO.NotificationViewDto;
+import CDProject.vfmarket.domain.entity.Item;
 import CDProject.vfmarket.domain.entity.Notification;
+import CDProject.vfmarket.domain.entity.User;
 import CDProject.vfmarket.exceptions.NotFoundException;
+import CDProject.vfmarket.repository.ItemRepository;
 import CDProject.vfmarket.repository.NotificationRepository;
 import CDProject.vfmarket.repository.UserRepository;
 import java.util.List;
@@ -16,17 +19,22 @@ public class NotificationService {
     private final NotificationRepository notificationRepository;
 
     private final UserRepository userRepository;
+    private final ItemRepository itemRepository;
 
 
     public List<NotificationViewDto> getNotifications(Long userId) {
         return notificationRepository.findNotificationsByUserId(userId);
     }
 
-    public void makeNotification(Long userId, String message) {
+    public void makeNotification(Long userId, Long itemId, String message) {
+
+        User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException("해당 유저를 찾을 수 없습니다."));
+        Item item = itemRepository.findById(itemId).orElseThrow(() -> new NotFoundException("해당 상를 찾을 수 없습니다."));
         Notification notification = Notification.builder().
-                user(userRepository.findById(userId).get())
+                user(user)
                 .isRead(false)
                 .message(message)
+                .item(item)
                 .build();
         notificationRepository.save(notification);
     }
