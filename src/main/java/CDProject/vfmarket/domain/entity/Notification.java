425ced3ad0c09f42065cad48f6459f830e1f11f6
leedonghyun <skydh507@ajou.ac.kr>
2023-11-30 05:44:19 +0900

    refactor : 도메인 수정

--- src/main/java/CDProject/vfmarket/domain/entity/Notification.java
+++ src/main/java/CDProject/vfmarket/domain/entity/Notification.java
@@ -1,8 +1,10 @@
 package CDProject.vfmarket.domain.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -22,7 +24,8 @@ import lombok.Setter;
 public class Notification extends BaseTimeEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notification_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
