dcc9dc8063c29a3bb29025dda5907bde0e314a54
leedonghyun <skydh507@ajou.ac.kr>
2023-11-30 05:39:22 +0900

    fix : @Param 에러 해결

--- src/main/java/CDProject/vfmarket/repository/ReviewRepository.java
+++ src/main/java/CDProject/vfmarket/repository/ReviewRepository.java
@@ -5,18 +5,19 @@ import CDProject.vfmarket.domain.entity.Review;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+결import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     Review findByItem_Id(Long itemId);
 
-    @Query("select new CDProject.vfmarket.domain.dto.ReviewDTO.ReviewDto(r.id,b.id,b.name,r.title,r.content,r.writeStatus,,r.lastModifiedDate)"
+    @Query("select new CDProject.vfmarket.domain.dto.ReviewDTO.ReviewDto(r.id,b.id,b.name,r.starRate,r.title,r.content,r.writeStatus,r.lastModifiedDate)"
             + " from Review r"
             + " left join r.item i"
             + " left join r.comments c"
             + " left join r.seller s"
             + " left join r.buyer b"
-            + " where u.id = :sellerId")
-    List<ReviewDto> findALlReviewBySellerId(Long sellerId);
+            + " where s.id = :sellerId")
+    List<ReviewDto> findALlReviewBySellerId(@Param("sellerId") Long sellerId);
 
 }
