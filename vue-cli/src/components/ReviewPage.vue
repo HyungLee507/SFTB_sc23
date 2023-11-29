1d8be33623ad3ebc58105706960c5dcc3a83d636
leedonghyun <skydh507@ajou.ac.kr>
2023-11-30 05:43:42 +0900

    feat : 리뷰 페이지

--- vue-cli/src/components/ReviewPage.vue
+++ vue-cli/src/components/ReviewPage.vue
@@ -1,64 +1,75 @@
-
 <template>
-    <div>
-        <h1 class="text-center">후기 작성</h1>
-        <form @submit.prevent="saveReview" class="p-4">
-            <div class="form-group">
-                <label for="title">제목:</label>
-                <input type="text" id="title" v-model="review.title" required class="form-control">
-            </div>
-            <div class="form-group">
-                <label for="score">판매자에 대한 점수:</label>
-                <select id="score" v-model="review.score" required class="form-control">
-                    <option value="1">1점</option>
-                    <option value="2">2점</option>
-                    <option value="3">3점</option>
-                    <option value="4">4점</option>
-                    <option value="5">5점</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="content">내용:</label>
-                <textarea id="content" v-model="review.content" required class="form-control"></textarea>
-            </div>
-            <button type="submit" class="btn btn-primary">저장</button>
-        </form>
-    </div>
+  <div>
+    <h1 class="text-center" style="margin-top: 30px">상품 리뷰</h1>
+    <form @submit.prevent="saveReview" class="p-4 d-flex flex-column align-items-center">
+      <div class="form-group w-60">
+        <label for="title" style="font-weight: bold">제목:</label>
+        <input type="text" id="title" v-model="review.title" required class="form-control">
+      </div>
+      <div class="form-group w-70">
+        <label for="rating" style="font-weight: bold">평가:</label>
+        <star-rating
+            id="rating"
+            v-model="review.starRate"
+            required
+            :show-rating="false"
+            :star-size="30"
+        ></star-rating>
+      </div>
+      <div class="form-group w-50">
+        <label for="content" style="font-weight: bold">내용:</label>
+        <textarea id="content" v-model="review.content" required class="form-control"></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary">저장</button>
+    </form>
+  </div>
 </template>
 
 <script>
 import axios from 'axios';
+//npm install vue-star-rating
+import StarRating from 'vue-star-rating';
 
 export default {
-    data() {
-        return {
-            review: {
-                title: '',
-                score: '',
-                content: ''
-            }
-        };
+  components: {
+    StarRating,
+  },
+  data() {
+    return {
+      review: {
+        itemId: 0,
+        title: '',
+        starRate: 0,
+        content: '',
+      },
+    };
+  },
+
+  methods: {
+    saveReview() {
+      // const productId = this.$route.params.id;
+      this.review.itemId = this.$route.params.id;
+      axios
+          .post("/review-upload", {
+            itemId: this.review.itemId,
+            title: this.review.title,
+            starRate: this.review.starRate,
+            content: this.review.content,
+
+          })
+          .then(response => {
+            alert('후기가 저장되었습니다!');
+            this.$router.push({name: 'productinformationPage', params: {id: this.$route.params.id}});
+            console.log(response.data);
+          })
+          .catch(error => {
+            alert('후기 저장에 실패했습니다!');
+            console.error(error);
+          })
+          .finally(() => {
+            this.$bvModal.hide('reviewModal');
+          });
     },
-    methods: {
-        saveReview() {
-            axios
-                .post('/api/review', {
-                    title: this.review.title,
-                    score: this.review.score,
-                    content: this.review.content,
-                    item_id: this.$route.params.id
-                })
-                .then(response => {
-                    alert('후기가 저장되었습니다!');
-                    this.$router.push({ name: 'productinformationPage', params: { id: this.$route.params.id } });
-                    console.log(response.data);
-                })
-                .catch(error => {
-                    alert('후기 저장에 실패했습니다!');
-                    console.error(error);
-                });
-        }
-    }
+  },
 };
 </script>
-
