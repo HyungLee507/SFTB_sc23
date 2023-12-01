dd9c5fba773570b5324b18ab5faf698805020521
leedonghyun <skydh507@ajou.ac.kr>
2023-12-02 06:43:07 +0900

    add : 운송장 번호 조회, 배송 정보 조회 구현

--- src/main/java/CDProject/vfmarket/repository/OrderDetailRepository.java
+++ src/main/java/CDProject/vfmarket/repository/OrderDetailRepository.java
@@ -2,6 +2,20 @@ package CDProject.vfmarket.repository;
 
 import CDProject.vfmarket.domain.entity.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
+
+    @Query("select od.billingNumber from OrderDetail od"
+            + " left join od.order o"
+            + " where o.id = :orderId ")
+    String findBillingNumberById(@Param("orderId") Long orderId);
+
+
+    @Query("select od.buyer_addr from OrderDetail od"
+            + " left join od.order o"
+            + " where o.id = :orderId ")
+    String findAddressById(@Param("orderId") Long orderId);
+
 }
