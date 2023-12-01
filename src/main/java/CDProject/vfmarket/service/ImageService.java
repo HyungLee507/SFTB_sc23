b2d1bb6530816b4496b3b355984a47fd60e83e4a
leedonghyun <skydh507@ajou.ac.kr>
2023-12-02 06:44:49 +0900

    feat : 이미지 수정 기능

---
+++ src/main/java/CDProject/vfmarket/service/ImageService.java
@@ -0,0 +1,56 @@
+package CDProject.vfmarket.service;
+
+import CDProject.vfmarket.domain.entity.Image;
+import CDProject.vfmarket.repository.ImageRepository;
+import jakarta.transaction.Transactional;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ImageService {
+
+    private final ImageRepository imageRepository;
+
+    private final String uploadFolder = "/Users/leedonghyun/Desktop/images";
+
+    private final Path rootLocation = Paths.get("/Users/leedonghyun/Desktop/images");
+
+
+    public void updateImage(Long imageId, MultipartFile file) throws IOException {
+        Image image = imageRepository.findById(imageId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid image ID: " + imageId));
+
+        String uploadFileName = file.getOriginalFilename();
+        String uuid = UUID.randomUUID().toString();
+        uploadFileName = uuid + "_" + uploadFileName;
+        File saveFile = new File(uploadFolder, uploadFileName);
+        file.transferTo(saveFile);
+
+        deleteExistingFile(image.getFileName());
+
+        image.setFileName(uploadFileName);
+
+    }
+
+    private void deleteExistingFile(String fileName) {
+        if (fileName != null && !fileName.isEmpty()) {
+            try {
+                Path file = rootLocation.resolve(fileName);
+                Files.deleteIfExists(file);
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to delete existing file: " + fileName, e);
+            }
+        }
+    }
+}
