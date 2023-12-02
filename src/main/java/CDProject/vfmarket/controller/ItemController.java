28a518218e14c463ff305347ee617ad86ba09c8b
leedonghyun <skydh507@ajou.ac.kr>
2023-12-03 00:20:29 +0900

    refactor : 주석 제거 및 인증 정보 로직 사용

--- src/main/java/CDProject/vfmarket/controller/ItemController.java
+++ src/main/java/CDProject/vfmarket/controller/ItemController.java
@@ -1,24 +1,28 @@
 package CDProject.vfmarket.controller;
 
+import static CDProject.vfmarket.global.AuthenticationUserId.getAuthenticatedUser;
+
 import CDProject.vfmarket.domain.dto.itemDTO.ItemDetailDto;
 import CDProject.vfmarket.domain.dto.itemDTO.ItemViewDto;
 import CDProject.vfmarket.domain.dto.itemDTO.SalesItemDto;
-import CDProject.vfmarket.global.jwt.TokenValueProvider;
+import CDProject.vfmarket.service.ImageService;
 import CDProject.vfmarket.service.ItemService;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @RestController
@@ -27,20 +31,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class ItemController {
 
     private final ItemService itemService;
-    private final TokenValueProvider tokenValueProvider;
+    private final ImageService imageService;
 
     @GetMapping("/product-list")
-//    @PreAuthorize("isAuthenticated()")
     public List<ItemViewDto> productList() {
         List<ItemViewDto> allItemsWithImages = itemService.productList();
-
         Map<Long, ItemViewDto> uniqueItemsMap = new HashMap<>();
         for (ItemViewDto dto : allItemsWithImages) {
             if (!uniqueItemsMap.containsKey(dto.getId())) {
                 uniqueItemsMap.put(dto.getId(), dto);
             }
         }
-        log.info("value is = {}", uniqueItemsMap.values());
         return new ArrayList<>(uniqueItemsMap.values());
     }
 
@@ -56,11 +57,9 @@ public class ItemController {
 
     @GetMapping("/salesItems")
     @PreAuthorize("isAuthenticated()")
-    public List<SalesItemDto> salesItems(@RequestHeader("Authorization") String token) {
-        Long sellerId = tokenValueProvider.extractUserId(token);
-        List<SalesItemDto> salesItems = itemService.getSalesItems(sellerId);
-        log.info("sales Items ={}", salesItems);
-        return salesItems;
+    public List<SalesItemDto> salesItems() {
+        Long sellerId = getAuthenticatedUser();
+        return itemService.getSalesItems(sellerId);
     }
 
 
@@ -73,5 +72,18 @@ public class ItemController {
             return "아이템 삭제 실패";
         }
     }
+
+    @PutMapping("/updateItem/image/{imageId}")
+    public ResponseEntity<?> updateImage(@PathVariable Long imageId, @RequestParam("file") MultipartFile file) {
+        try {
+            imageService.updateImage(imageId, file);
+            return ResponseEntity.ok().build();
+        } catch (IOException e) {
+            return ResponseEntity.status(500).body("File storage error");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
 
 }
