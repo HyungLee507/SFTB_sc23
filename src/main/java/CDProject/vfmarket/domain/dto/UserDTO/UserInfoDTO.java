6477ebafbd8559422ba6e4e480f905f9169ce7cc
leedonghyun <skydh507@ajou.ac.kr>
2023-12-02 16:17:38 +0900

    add : 유저 정보 dto

---
+++ src/main/java/CDProject/vfmarket/domain/dto/UserDTO/UserInfoDTO.java
@@ -0,0 +1,13 @@
+package CDProject.vfmarket.domain.dto.UserDTO;
+
+import lombok.Data;
+
+@Data
+public class UserInfoDTO {
+
+    private String userName;
+
+    private String email;
+
+    private Integer size;
+}
