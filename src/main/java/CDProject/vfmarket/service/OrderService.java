8c539e5fb301f72694c66a114592ae842616faf8
leedonghyun <skydh507@ajou.ac.kr>
2023-12-03 00:23:35 +0900

    feat : 구매 완료 내역 조회

--- src/main/java/CDProject/vfmarket/service/OrderService.java
+++ src/main/java/CDProject/vfmarket/service/OrderService.java
@@ -1,12 +1,17 @@
 package CDProject.vfmarket.service;
 
+import CDProject.vfmarket.domain.dto.OrderDTO.OrderDetailSaveDTO;
+import CDProject.vfmarket.domain.dto.OrderDTO.OrderDoneDTO;
 import CDProject.vfmarket.domain.dto.OrderDTO.OrderViewDto;
 import CDProject.vfmarket.domain.dto.OrderDTO.ProgressOrderDto;
 import CDProject.vfmarket.domain.dto.OrderDTO.SoldItemViewDto;
+import CDProject.vfmarket.domain.entity.Item;
+import CDProject.vfmarket.domain.entity.ItemStatus;
 import CDProject.vfmarket.domain.entity.Order;
 import CDProject.vfmarket.domain.entity.OrderDetail;
 import CDProject.vfmarket.domain.entity.OrderStatus;
 import CDProject.vfmarket.exceptions.NotFoundException;
+import CDProject.vfmarket.repository.ItemRepository;
 import CDProject.vfmarket.repository.OrderDetailRepository;
 import CDProject.vfmarket.repository.OrderRepository;
 import jakarta.transaction.Transactional;
@@ -21,6 +26,7 @@ import org.springframework.stereotype.Service;
 @Transactional
 @RequiredArgsConstructor
 public class OrderService {
+    private final ItemRepository itemRepository;
 
 
     private final OrderRepository orderRepository;
@@ -45,15 +51,27 @@ public class OrderService {
         return progressOrderDtos;
     }
 
-    //Todo
-    public void updateBillingNumber(Long orderId, String billingNumber) {
-        Order order = orderRepository.findById(orderId).orElseThrow(() -> new NotFoundException("해당 상를 찾을 수 없습니다."));
+    public void updateBillingNumber(OrderDetailSaveDTO orderDetailSaveDTO) {
+        Order order = orderRepository.findById(orderDetailSaveDTO.getOrderId())
+                .orElseThrow(() -> new NotFoundException("해당 주문을 찾을 수 없습니다."));
         OrderDetail orderDetail = order.getOrderDetail();
-        log.info("sdlkfjsd = {}, {}", orderId, billingNumber);
-        orderDetail.setBillingNumber(billingNumber);
+        orderDetail.setBillingNumber(orderDetailSaveDTO.getBillingNumber());
+        orderDetail.setDeliveryCompany(orderDetailSaveDTO.getDeliveryCompany());
         order.setStatus(OrderStatus.DEAL_DONE);
+
+        Item item = itemRepository.findById(order.getItem().getId())
+                .orElseThrow(() -> new NotFoundException("해당 상품을 찾을수 없습니다."));
+        item.setStatus(ItemStatus.SOLD_OUT);
+
+        itemRepository.save(item);
         orderDetailRepository.save(orderDetail);
         orderRepository.save(order);
+
+    }
+
+    public List<OrderDoneDTO> getDoneOrders(Long buyerId) {
+        return orderRepository.orderDoneData(buyerId);
+
     }
 
 
