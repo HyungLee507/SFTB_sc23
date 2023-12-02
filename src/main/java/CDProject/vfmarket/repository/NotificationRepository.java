4e7acb661c723586b56d817aed2502c01db8b4df
leedonghyun <skydh507@ajou.ac.kr>
2023-12-03 00:21:00 +0900

    fix : db오류 해결위한 연관관계 삭제

--- src/main/java/CDProject/vfmarket/repository/NotificationRepository.java
+++ src/main/java/CDProject/vfmarket/repository/NotificationRepository.java
@@ -9,10 +9,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    @Query("select new CDProject.vfmarket.domain.dto.NotificationDTO.NotificationViewDto(n.message,n.createdDate,n.isRead,i.id)"
+    @Query("select new CDProject.vfmarket.domain.dto.NotificationDTO.NotificationViewDto(n.message,n.createdDate,n.isRead)"
             + " from Notification n"
             + " left join n.user u"
-            + " left join n.item i"
+//            + " left join n.item i"
             + " where u.id = :userId")
     List<NotificationViewDto> findNotificationsByUserId(@Param("userId") Long userId);
 }
