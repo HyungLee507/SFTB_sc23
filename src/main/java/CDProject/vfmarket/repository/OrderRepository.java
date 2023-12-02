30601dade3db64da44e1f21b06abbeb4a9d9d217
leedonghyun <skydh507@ajou.ac.kr>
2023-12-03 00:23:23 +0900

    feat : 구매 완료 내역 조회

--- src/main/java/CDProject/vfmarket/repository/OrderRepository.java
+++ src/main/java/CDProject/vfmarket/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package CDProject.vfmarket.repository;
 
+import CDProject.vfmarket.domain.dto.OrderDTO.OrderDoneDTO;
 import CDProject.vfmarket.domain.dto.OrderDTO.OrderViewDto;
 import CDProject.vfmarket.domain.dto.OrderDTO.SoldItemViewDto;
 import CDProject.vfmarket.domain.entity.Order;
@@ -16,7 +17,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     List<Order> findByBuyer_Id(Long buyerId);
 
-    @Query("SELECT new CDProject.vfmarket.domain.dto.OrderDTO.OrderViewDto(o.id,i.itemName, o.representativeImage, i.price,i.id,i.shoeSize,o.lastModifiedDate) FROM Order o JOIN o.item i WHERE o.buyer.id = :buyerId AND o.status = CDProject.vfmarket.domain.entity.OrderStatus.COMPLETE_PAYMENT")
+    @Query("SELECT new CDProject.vfmarket.domain.dto.OrderDTO.OrderViewDto(o.id,i.itemName, o.representativeImage, i.price,i.id,i.shoeSize,o.lastModifiedDate) FROM Order o JOIN o.item i WHERE o.buyer.id = :buyerId AND o.status = CDProject.vfmarket.domain.entity.OrderStatus.TRANSACTION_IN")
     List<OrderViewDto> orderViewData(@Param("buyerId") Long buyerId);
 
     @Query("SELECT new CDProject.vfmarket.domain.dto.OrderDTO.SoldItemViewDto(o.id,i.itemName, o.representativeImage, i.price,i.shoeSize,o.lastModifiedDate) FROM Order o JOIN o.item i WHERE o.sellerId = :sellerId AND o.status = CDProject.vfmarket.domain.entity.OrderStatus.DEAL_DONE")
@@ -24,5 +25,11 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("SELECT new CDProject.vfmarket.domain.dto.OrderDTO.OrderViewDto(o.id,i.itemName, o.representativeImage, i.price,i.id,i.shoeSize,o.lastModifiedDate) FROM Order o JOIN o.item i WHERE o.sellerId = :sellerId AND o.status = CDProject.vfmarket.domain.entity.OrderStatus.TRANSACTION_IN")
     List<OrderViewDto> progressOrderData(@Param("sellerId") Long sellerId);
+
+    @Query("SELECT new CDProject.vfmarket.domain.dto.OrderDTO.OrderDoneDTO(o.id,i.itemName, o.representativeImage, i.price,i.id,i.shoeSize,o.lastModifiedDate,i.reviewSubmitted,od.deliveryCompany,od.billingNumber) "
+            + "FROM Order o JOIN o.item i "
+            + "JOIN o.orderDetail od"
+            + " WHERE o.buyer.id = :buyerId AND o.status = CDProject.vfmarket.domain.entity.OrderStatus.DEAL_DONE")
+    List<OrderDoneDTO> orderDoneData(@Param("buyerId") Long buyerId);
 
 }
