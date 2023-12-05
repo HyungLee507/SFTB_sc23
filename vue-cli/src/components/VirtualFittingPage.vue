6032628094ac05bb2c42792aec9edc6321a8dfee
bioroid17 <trace1997@ajou.ac.kr>
2023-12-05 20:14:36 +0900

    VirtualFittingPage.vue 수정

--- vue-cli/src/components/VirtualFittingPage.vue
+++ vue-cli/src/components/VirtualFittingPage.vue
@@ -16,15 +16,26 @@
             <b-card-group deck>
               <div v-for="styleShot in styleShots" :key="styleShot.styleShotId">
                 <b-card :title="styleShot.itemName" :img-src="getImageUrl(styleShot.styleShotName)" img-alt="Image"
-                  img-top class="fixed-card-size" @click="redirectToProductList(styleShot.itemId)">
+                        img-top class="fixed-card-size" @click="openModal(styleShot)">
                 </b-card>
-                <b-button variant="danger" @click="deleteProduct(styleShot.styleShotId)">삭제</b-button>
               </div>
             </b-card-group>
           </div>
         </b-col>
       </b-row>
     </b-container>
+    <b-modal v-model="modalShow" @ok="deleteProduct(styleShotId)" @cancel="redirectToProductList(itemId)">
+      <template v-slot:modal-title>
+        삭제 또는 상품페이지로 이동
+      </template>
+      <template v-slot:default>
+        <p>카드를 삭제하거나 상품페이지로 이동하시겠습니까?</p>
+      </template>
+      <template v-slot:modal-footer>
+        <b-button variant="danger" size="sm" @click="deleteProduct(styleShotId)">삭제</b-button>
+        <b-button size="sm" @click="redirectToProductList(itemId)">상품페이지로 이동</b-button>
+      </template>
+    </b-modal>
   </div>
 </template>
 
@@ -39,6 +50,9 @@ export default {
   data() {
     return {
       styleShots: [],
+      modalShow: false,
+      styleShotId: null,
+      itemId: null,
     };
   },
   created() {
@@ -58,31 +72,38 @@ export default {
   methods: {
     fetchProducts() {
       axios
-        .get('/vf/viewstyle')
-        .then((response) => {
-          this.styleShots = response.data;
-          console.log(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+          .get('/vf/viewstyle')
+          .then((response) => {
+            this.styleShots = response.data;
+            console.log(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
     },
     getImageUrl(imageName) {
       return `http://localhost:8080/vf/${imageName}`;
     },
+    openModal(styleShot) {
+      this.styleShotId = styleShot.styleShotId;
+      this.itemId = styleShot.itemId;
+      this.modalShow = true;
+    },
     deleteProduct(styleShotId) {
       axios
-        .delete(`/vf/deletestyle?styleShotId=${styleShotId}`)
-        .then((response) => {
-          console.log(response.data);
-          this.fetchProducts();
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+          .delete(`/vf/deletestyle?styleShotId=${styleShotId}`)
+          .then((response) => {
+            console.log(response.data);
+            this.fetchProducts();
+            this.modalShow = false;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
     },
     redirectToProductList(itemId) {
       this.$router.push(`/product-list/${itemId}`);
+      this.modalShow = false;
     },
   },
 };
